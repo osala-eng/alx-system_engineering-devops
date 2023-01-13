541b72f2f21461fda6d527358cc4fa4d94bd26e2
Jashon Osala <jashonosala@gmail.com>
2023-01-13 15:50:03 +0300

    Puppet config

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,33 +1,41 @@
-$redirect_me = '\\\tlocation redirect_me/ {\n\t\treturn 301 https://youtu.be/mlUOUncSaTk;\n\t}\n'
-$error_page = 'error_page 404 /404.html'
-$default_site = '/etc/nginx/sites-available/default'
-generate_number = "/usr/bin/grep -Fn location ${default_site} | /usr/bin/head -1 | cut -d'-', -f1"
+$default_site_loc = '/etc/nginx/sites-available/default'
+$default_site = 'https://raw.githubusercontent.com/osala-eng/alx-system_engineering-devops/master/0x0C-web_server/default-site'
 
 # Run apt-get update
 exec { 'apt-update':
   command => '/usr/bin/apt-get update'
 }
 
+# Install nginx
 package { 'nginx':
+  ensure  => installed,
   require => Exec['apt-update'],
-  ensure  => installed
 }
 
+# Create a new index.html
 file { 'Index.html':
   require => Package['nginx'],
   path    => '/var/www/index.html',
   content => 'Hello World!'
 }
 
+# Create a new error page
 file { '/var/www/404.html':
   ensure  => file,
   require => Package['nginx'],
-  content => 'Ceci n'est pas une page'
+  content => 'Ceci n\'est pas une page'
 }
 
+# Replace default site config
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
   require => Package['nginx']
-}-> exec { 'Create redirect_me endpoint':
-  command => '/usr/bin/sed -i "48i $redirect_me" /etc/nginx/sites-available/default'
+}-> exec { 'Replace config':
+  command => "/usr/bin/curl ${default_site} > ${default_site_loc}"
+}
+
+# Start nginx service
+service { 'nginx':
+  ensure  => running,
+  require => Exec['Replace config'],
 }
