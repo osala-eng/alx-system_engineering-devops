815eb6b1d0774b497771f55107459142fbbff991
Jashon Osala <jashonosala@gmail.com>
2023-01-19 00:43:14 +0300

    Puppet file

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,43 @@
+$default_site_loc = '/etc/nginx/sites-available/default'
+$default_site = 'https://raw.githubusercontent.com/osala-eng/alx-system_engineering-devops/master/0x0F-load_balancer/default-site'
+
+# Run apt-get update
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update'
+}
+
+# Install nginx
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-update'],
+}
+
+# Create a new index.html
+file { 'Create index.html':
+  require => Package['nginx'],
+  path    => '/var/www/html/index.html',
+  content => 'Hello World!\n'
+}
+
+# Create a new error page
+file { 'Create 404.html':
+  require => Package['nginx'],
+  path    => '/var/www/html/404.html',
+  content => 'Ceci n\'est pas une page\n'
+}
+
+# Replace default site config
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  require => Package['nginx']
+}-> exec { 'Replace config':
+  command => "/usr/bin/curl ${default_site} > ${default_site_loc}"
+}-> exec { 'Add value of host name':
+  command => "/usr/bin/sed -i \"s/x-served-by-hostname/\$(hostname)/g" ${default_site_loc}"
+}
+
+# Start nginx service
+service { 'nginx':
+  ensure  => running,
+  require => Exec['Add value of host name'],
+}
